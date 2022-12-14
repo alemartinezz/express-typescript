9baf9cedb3240c5ccee76c4f17ac0054ccf7fd0a
amartinez-hellohello <alejandro.martinez@hellohello.is>
2022-12-14 10:04:10 -0300

    update

--- common/common.routes.config.ts
+++ common/common.routes.config.ts
@@ -8,8 +8,10 @@ export abstract class CommonRoutesConfig {
     this.name = name;
     this.configureRoutes();
   }
+
   getName() {
     return this.name;
   }
+
   abstract configureRoutes(): express.Application;
 }
